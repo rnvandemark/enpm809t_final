d65a3d18618fa573feb5ee90a3f27869c73b9990
R. Nick Vandemark <rnvandemark@gmail.com>
2023-04-28 22:50:38 -0400

    Add a bufferless IMU class, to quickly get the most recent IMU reading.

---
+++ bufferless_imu.py
@@ -0,0 +1,26 @@
+from numpy import pi, deg2rad
+from serial import Serial
+from time import sleep
+
+def fix_orientation(degs):
+    return -deg2rad(degs) + (2 * pi)
+
+class BufferlessImu(object):
+    def __init__(self, imu_port, baud_rate):
+        self.ser = Serial(imu_port, baud_rate)
+        self.count = 0
+        self.last_sample = None
+
+    def read(self):
+        while (self.last_sample is None) or (self.ser.in_waiting > 0):
+            if (self.ser.in_waiting <= 0) and (self.last_sample is None):
+                sleep(0.05)
+            else:
+                self.count += 1
+                line_original = str(self.ser.readline())
+                if self.count > 10:
+                    try:
+                        self.last_sample = fix_orientation(float(''.join(c for c in line_original if c in "0123456789.")))
+                    except:
+                        pass
+        return self.last_sample
